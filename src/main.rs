7230e26004484fc759ffdfd7c26b77f8f841a51c
Alex <alex@fischman.org>
2021-12-07 22:44:23 -0800

    Moved to projection rendering of 3D GA

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,40 @@
 mod vector;
 
 use skulpin::skia_safe::*;
-use vector::{Vector, I, X, Y};
+use vector::{Bivector, Vector, E1, E12, E2, E23, E3, E31};
 
 fn main() {
-	assert_eq!(X ^ Y, I);
-	assert_eq!(Y ^ X, -1.0 * I);
+	assert_eq!(E1 - E1, E2 - E2);
+	assert_eq!(E1 | E2, 0.0);
+	assert_eq!(E2 ^ E3, E23);
+	assert_eq!(E2 ^ E1, -E12);
+	assert_eq!(!E1, -E23);
+	assert_eq!(!E31, -E2);
+	assert_eq!(E31 | E31, -1.0);
+	assert_eq!(E31 | E23, 0.0);
+	assert_eq!(E23 & E12, E2);
+
+	let v = |x, y| x * E1 + y * E2 + E3;
+	let label = |s: &str, x, y| Some((s.to_string(), Point::new(x, y)));
+
+	let a = v(1.0, 2.0);
+	let b = v(-2.0, 3.0);
+	let c = v(-4.0, -3.0);
+
+	let ab = a ^ b;
+	let bc = b ^ c;
+	let ca = c ^ a;
 
 	let scale = 5.0;
-	let vs = vec![
-		(X, Color::RED, Some(("x".to_string(), (0.1 * Y).to_point()))),
-		(
-			Y,
-			Color::BLUE,
-			Some(("y".to_string(), (-0.1 * X).to_point())),
-		),
-		(X - Y, Color::GREEN, None),
+	let vectors = vec![
+		(a, Color::RED, label("a", 0.0, 0.2)),
+		(b, Color::GREEN, label("b", 0.0, -0.2)),
+		(c, Color::BLUE, label("c", 0.2, 0.0)),
+	];
+	let bivectors = vec![
+		(ab, Color::RED, label("ab", -0.2, 2.6)),
+		(bc, Color::GREEN, label("bc", -3.2, 0.2)),
+		(ca, Color::BLUE, label("ca", -1.2, 0.2)),
 	];
 
 	skulpin::app::AppBuilder::new()
@@ -23,11 +42,13 @@ fn main() {
 			Rect::new(-scale, scale, scale, -scale),
 			skulpin::skia_safe::matrix::ScaleToFit::Center,
 		))
-		.run(App { vs });
+		.window_title("Geometric Algebra")
+		.run(App { vectors, bivectors });
 }
 
 struct App {
-	vs: Vec<(Vector, Color, Option<(String, Point)>)>,
+	vectors: Vec<(Vector, Color, Option<(String, Point)>)>,
+	bivectors: Vec<(Bivector, Color, Option<(String, Point)>)>,
 }
 
 impl skulpin::app::AppHandler for App {
@@ -69,28 +90,50 @@ impl skulpin::app::AppHandler for App {
 		canvas.draw_line(Point::new(0.0, b.bottom), Point::new(0.0, b.top), &paint);
 
 		paint.set_stroke_width(0.025);
-		for (v, color, label) in &self.vs {
-			paint.set_style(paint::Style::Stroke);
+		for (bv, color, label) in &self.bivectors {
 			paint.set_color(*color);
-			let tip = v.to_point();
-			canvas.draw_line(Point::new(0.0, 0.0), tip, &paint);
-			let a = f32::atan2(tip.y, tip.x) + 0.5;
-			canvas.draw_line(tip, tip - Point::new(a.cos(), a.sin()).scaled(0.1), &paint);
-			let a = f32::atan2(tip.y, tip.x) - 0.5;
-			canvas.draw_line(tip, tip - Point::new(a.cos(), a.sin()).scaled(0.1), &paint);
-			if let Some((string, offset)) = label {
-				paint.set_style(paint::Style::Fill);
-				let origin = canvas.local_to_device_as_3x3().map_point(tip / 2.0 + *offset);
-				let r = font.measure_str(string, Some(&paint)).1;
-				let offset = Point::new(r.width(), -r.height()) / 2.0;
-				canvas.save();
-				canvas.reset_matrix();
-				canvas.draw_str(string, origin - offset, &font, &paint);
-				canvas.restore();
+			let lt = b.left() * E1 + b.top() * E2 + E3;
+			let rt = b.right() * E1 + b.top() * E2 + E3;
+			let lb = b.left() * E1 + b.bottom() * E2 + E3;
+			let rb = b.right() * E1 + b.bottom() * E2 + E3;
+			let ps: Vec<Point> = vec![lt ^ rt, lb ^ rb, lt ^ lb, rt ^ rb]
+				.into_iter()
+				.map(|edge| edge & *bv)
+				.filter_map(Vector::to_point)
+				.collect();
+			if ps.len() >= 2 {
+				canvas.draw_line(ps[0], ps[1], &paint);
+				if let Some((string, position)) = label {
+					paint.set_style(paint::Style::Fill);
+					let origin = canvas.local_to_device_as_3x3().map_point(*position);
+					let r = font.measure_str(string, Some(&paint)).1;
+					let offset = Point::new(r.width(), -r.height()) / 2.0;
+					canvas.save();
+					canvas.reset_matrix();
+					canvas.draw_str(string, origin - offset, &font, &paint);
+					canvas.restore();
+					paint.set_style(paint::Style::Stroke);
+				}
 			}
 		}
 
 		paint.set_style(paint::Style::Fill);
+		for (v, color, label) in &self.vectors {
+			if let Some(tip) = v.to_point() {
+				paint.set_color(*color);
+				canvas.draw_circle(tip, 0.05, &paint);
+				if let Some((string, position)) = label {
+					let origin = canvas.local_to_device_as_3x3().map_point(tip + *position);
+					let r = font.measure_str(string, Some(&paint)).1;
+					let offset = Point::new(r.width(), -r.height()) / 2.0;
+					canvas.save();
+					canvas.reset_matrix();
+					canvas.draw_str(string, origin - offset, &font, &paint);
+					canvas.restore();
+				}
+			}
+		}
+
 		paint.set_color(Color::BLACK);
 		canvas.draw_circle(Point::new(0.0, 0.0), 0.05, &paint);
 	}
--- src/vector.rs
+++ src/vector.rs
@@ -2,118 +2,147 @@ use std::ops::*;
 
 type Scalar = f32;
 
-trait Geometric
-where
-	Self: Sized + Add + Sub + Div<Scalar>,
-	Scalar: Mul<Self>,
-{
-	fn inv(self) -> Self;
-}
-
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Vector {
-	x: Scalar,
-	y: Scalar,
+	e1: Scalar,
+	e2: Scalar,
+	e3: Scalar,
 }
 
-pub const X: Vector = Vector { x: 1.0, y: 0.0 };
-pub const Y: Vector = Vector { x: 0.0, y: 1.0 };
+pub const E1: Vector = Vector { e1: 1.0, e2: 0.0, e3: 0.0 };
+pub const E2: Vector = Vector { e1: 0.0, e2: 1.0, e3: 0.0 };
+pub const E3: Vector = Vector { e1: 0.0, e2: 0.0, e3: 1.0 };
 
-impl Add for Vector {
-	type Output = Self;
-	fn add(self, other: Self) -> Self::Output {
-		Self { x: self.x + other.x, y: self.y + other.y }
+impl Vector {
+	pub fn to_point(self) -> Option<skulpin::skia_safe::Point> {
+		if self.e3 == 0.0 {
+			None
+		} else {
+			Some(skulpin::skia_safe::Point::new(
+				self.e1 / self.e3,
+				self.e2 / self.e3,
+			))
+		}
 	}
 }
 
-impl Sub for Vector {
-	type Output = Self;
-	fn sub(self, other: Self) -> Self::Output {
-		Self { x: self.x - other.x, y: self.y - other.y }
+impl Add for Vector {
+	type Output = Vector;
+	fn add(self, other: Vector) -> Vector {
+		Vector { e1: self.e1 + other.e1, e2: self.e2 + other.e2, e3: self.e3 + other.e3 }
 	}
 }
 
 impl Mul<Vector> for Scalar {
 	type Output = Vector;
-	fn mul(self, other: Vector) -> Self::Output {
-		Vector { x: self * other.x, y: self * other.y }
+	fn mul(self, other: Vector) -> Vector {
+		Vector { e1: self * other.e1, e2: self * other.e2, e3: self * other.e3 }
 	}
 }
 
-impl Div<Scalar> for Vector {
-	type Output = Self;
-	fn div(self, other: Scalar) -> Self::Output {
-		Vector { x: self.x / other, y: self.y / other }
+impl Neg for Vector {
+	type Output = Vector;
+	fn neg(self) -> Vector {
+		Vector { e1: -self.e1, e2: -self.e2, e3: -self.e3 }
 	}
 }
 
-impl Geometric for Vector {
-	fn inv(self) -> Self {
-		self / self.norm()
+impl Sub for Vector {
+	type Output = Vector;
+	fn sub(self, other: Vector) -> Vector {
+		Vector { e1: self.e1 - other.e1, e2: self.e2 - other.e2, e3: self.e3 - other.e3 }
 	}
 }
 
 impl BitOr for Vector {
 	type Output = Scalar;
-	fn bitor(self, other: Self) -> Self::Output {
-		self.x * other.x + self.y * other.y
+	fn bitor(self, other: Vector) -> Scalar {
+		self.e1 * other.e1 + self.e2 * other.e2 + self.e3 * other.e3
 	}
 }
 
 impl BitXor for Vector {
 	type Output = Bivector;
-	fn bitxor(self, other: Vector) -> Self::Output {
-		(self.x * other.y - self.y * other.x) * I
+	fn bitxor(self, other: Vector) -> Bivector {
+		Bivector {
+			e23: self.e2 * other.e3 - self.e3 * other.e2,
+			e31: self.e3 * other.e1 - self.e1 * other.e3,
+			e12: self.e1 * other.e2 - self.e2 * other.e1,
+		}
 	}
 }
 
-impl Vector {
-	pub fn norm(self) -> Scalar {
-		self.x * self.x + self.y * self.y
-	}
-
-	pub fn to_point(self) -> skulpin::skia_safe::Point {
-		skulpin::skia_safe::Point::new(self.x, self.y)
+impl Not for Vector {
+	type Output = Bivector;
+	fn not(self) -> Bivector {
+		Bivector { e23: -self.e1, e31: -self.e2, e12: -self.e3 }
 	}
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Bivector {
-	i: Scalar,
+	e23: Scalar,
+	e31: Scalar,
+	e12: Scalar,
 }
 
-pub const I: Bivector = Bivector { i: 1.0 };
+pub const E23: Bivector = Bivector { e23: 1.0, e31: 0.0, e12: 0.0 };
+pub const E31: Bivector = Bivector { e23: 0.0, e31: 1.0, e12: 0.0 };
+pub const E12: Bivector = Bivector { e23: 0.0, e31: 0.0, e12: 1.0 };
 
 impl Add for Bivector {
-	type Output = Self;
-	fn add(self, other: Self) -> Self::Output {
-		Self { i: self.i + other.i }
-	}
-}
-
-impl Sub for Bivector {
-	type Output = Self;
-	fn sub(self, other: Self) -> Self::Output {
-		Self { i: self.i - other.i }
+	type Output = Bivector;
+	fn add(self, other: Bivector) -> Bivector {
+		Bivector {
+			e23: self.e23 + other.e23,
+			e31: self.e31 + other.e31,
+			e12: self.e12 + other.e12,
+		}
 	}
 }
 
 impl Mul<Bivector> for Scalar {
 	type Output = Bivector;
-	fn mul(self, other: Bivector) -> Self::Output {
-		Bivector { i: self * other.i }
+	fn mul(self, other: Bivector) -> Bivector {
+		Bivector { e23: self * other.e23, e31: self * other.e31, e12: self * other.e12 }
 	}
 }
 
-impl Div<Scalar> for Bivector {
-	type Output = Self;
-	fn div(self, other: Scalar) -> Self::Output {
-		Self { i: self.i / other }
+impl Neg for Bivector {
+	type Output = Bivector;
+	fn neg(self) -> Bivector {
+		Bivector { e23: -self.e23, e31: -self.e31, e12: -self.e12 }
 	}
 }
 
-impl Geometric for Bivector {
-	fn inv(self) -> Self {
-		-1.0 * self / self.i
+impl Sub for Bivector {
+	type Output = Bivector;
+	fn sub(self, other: Bivector) -> Bivector {
+		Bivector {
+			e23: self.e23 - other.e23,
+			e31: self.e31 - other.e31,
+			e12: self.e12 - other.e12,
+		}
+	}
+}
+
+impl BitOr for Bivector {
+	type Output = Scalar;
+	fn bitor(self, other: Bivector) -> Scalar {
+		-(self.e23 * other.e23 + self.e31 * other.e31 + self.e12 * other.e12)
+	}
+}
+
+impl BitAnd for Bivector {
+	type Output = Vector;
+	fn bitand(self, other: Bivector) -> Vector {
+		!((!self) ^ (!other))
+	}
+}
+
+impl Not for Bivector {
+	type Output = Vector;
+	fn not(self) -> Vector {
+		Vector { e1: -self.e23, e2: -self.e31, e3: -self.e12 }
 	}
 }
